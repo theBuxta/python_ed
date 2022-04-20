36540a0045dec1cd797e6f24b10146d248d0e26e
Maxim <the.buxta@yandex.ru>
2022-04-20 10:38:51 +0300

    Закончил лекцию 1

---
+++ lectures_python/hello_lecture_1.py
@@ -0,0 +1,74 @@
+print ("Hello, World!")
+print ()
+
+# ТИПЫ ДАННЫХ И ПЕРЕМЕННЫЕ
+
+# int, float, boolean, str, list, None
+# value = None
+# a = 1
+# b = 1.5
+# c = True
+# d = "Hello, World!"
+# e = [3,6,9]
+# print (f"int = {a}, float = {b}, boolean = {c}, string = {d}, list = {e}")
+
+# ВВОД И ВЫВОД ДАННЫХ
+
+# print("input A")
+# a = int(input())
+# print("input B")
+# b = int(input())
+# print(f"Result a + b = {a + b}")
+
+# АРИФМЕТИЧЕСКИЕ ОПЕРАЦИИ
+# +, -, *, /, %, //, **
+
+# print("input A")
+# a = int(input())
+# print("input B")
+# b = int(input())
+# c = a**b
+# print(f"{a} в степени {b} = {c}")
+
+# ЛОГИЧЕСКИЕ ОПЕРАЦИИ
+# >, >=, <, <=, ==, !=
+# not, and, or - не путать с &, |, ^
+# is, is not, in, not in
+#gen
+
+# a = 1 < 5
+# print (a)
+
+# УПРАВЛЯЮЩИЕ КОНСТРУКЦИИ 
+
+# if, if-else
+# a = int(input("a = "))
+# b = int(input("b = "))
+# if a > b:
+#     print (a)
+# else:
+#     print (b)
+
+# if, elif
+# username = input("Input name: ")
+# if username == "Sasha":
+#     print ("Hello, Sasha!")
+# elif username == "Pipironi":
+#     print("Hello, Pipironi ;)")
+# else:
+#     print("Hello, ", username)
+
+# while
+# original = 23
+# inverted = 0
+# while original != 0:
+#     inverted = inverted * 10 + (original % 10)
+#     original //= 10
+# else:
+#     print(inverted)
+
+# for
+# r = range(5)
+# for i in r:
+#     print(i)
+
